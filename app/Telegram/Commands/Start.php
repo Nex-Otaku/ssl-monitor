a8c23689c93f20ba8a262d9d49c81b0891b4c8a7
Nex Otaku <nex@otaku.ru>
2022-08-24 17:38:03 +0300

    Сделал приветствие бота

--- app/Telegram/Commands/Start.php
+++ app/Telegram/Commands/Start.php
@@ -2,6 +2,7 @@
 
 namespace App\Telegram\Commands;
 
+use App\Monitoring\Entities\MonitoringSite;
 use SergiX44\Nutgram\Nutgram;
 
 class Start
@@ -18,6 +19,25 @@ class Start
 
     public function __invoke(Nutgram $bot): void
     {
-        $bot->sendMessage('This is a command!');
+        $bot->sendMessage($this->getMessage($bot->userId()));
+    }
+
+    private function getMessage(int $userTgId): string
+    {
+        $message = "Бот следит за работой сайтов и оповещает вас о проблемах.\n\n";
+
+        $sites = MonitoringSite::forTgUser($userTgId);
+
+        if (count($sites) === 0) {
+            $message .= "Добавьте сайт в список, и бот начнёт его отслеживать.\n";
+        } else {
+            $message .= "Отслеживаемые сайты:\n";
+
+            foreach ($sites as $site) {
+                $message .= $site->getDomainName() . "\n";
+            }
+        }
+
+        return $message;
     }
 }
