f7960b2d4ae66b397f31722bbe7e3014e3f3e79c
Nex Otaku <nex@otaku.ru>
2022-08-25 14:39:46 +0300

    Добавил команду статуса

--- app/Telegram/Commands/Status.php
+++ app/Telegram/Commands/Status.php
@@ -2,7 +2,9 @@
 
 namespace App\Telegram\Commands;
 
+use App\Monitoring\Entities\MonitoringSite;
 use SergiX44\Nutgram\Nutgram;
+use SergiX44\Nutgram\Telegram\Attributes\ParseMode;
 
 class Status
 {
@@ -18,6 +20,57 @@ class Status
 
     public function __invoke(Nutgram $bot): void
     {
-        $bot->sendMessage('This is a command!');
+        $bot->sendMessage(
+            $this->getMessage($bot->userId()),
+            [
+                'parse_mode' => ParseMode::HTML,
+            ]
+        );
+    }
+
+    private function getMessage(int $userTgId): string
+    {
+        $sites = MonitoringSite::forTgUser($userTgId);
+
+        if (count($sites) === 0) {
+            return "Сайтов нет";
+        }
+
+        $okCount = 0;
+        $warningCount = 0;
+        $failCount = 0;
+
+        foreach ($sites as $site) {
+            if ($site->isOk()) {
+                $okCount++;
+            }
+
+            if ($site->isWarning()) {
+                $warningCount++;
+            }
+
+            if ($site->isFail()) {
+                $failCount++;
+            }
+        }
+
+        $total = count($sites);
+        $nodataCount = $total - $okCount - $warningCount - $failCount;
+
+        $okLabel = "✅ {$okCount}";
+
+        $warningLabel = ($warningCount > 0)
+            ? " / ⚠ {$warningCount}"
+            : '';
+
+        $failLabel = ($failCount > 0)
+            ? " / ❌ {$failCount}"
+            : '';
+
+        $nodataLabel = ($nodataCount > 0)
+            ? " / ❓ {$nodataCount}"
+            : '';
+
+        return "Сайты: {$okLabel}{$warningLabel}{$failLabel}{$nodataLabel}, всего {$total}";
     }
 }
